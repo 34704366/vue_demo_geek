ebf17f02c0221cbee8ceb2a00a1fc4e5c3b78750
wangch <34704366@qq.com>
2022-07-07 22:47:47 +0800

    新增todoitem

---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,26 @@
+<template>
+    <li class="show-item">
+        <input type="checkbox" v-model="isChecked">
+        <slot name="todo-item" :isChecked="isChecked" :time="time"></slot>
+    </li>
+</template>
+
+ 
+<script>
+export default {
+    props: ['item'],
+    data() {
+        return {
+            isChecked: false,
+            time: '2022/7/7 22:37:00'
+        }
+    },
+}
+</script>
+
+<style scoped>
+.show-item {
+    /* color: blue; */
+    font-size: 20px;
+}
+</style>
