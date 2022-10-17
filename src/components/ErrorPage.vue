90d07a4b987087aa97800e1b2133eb0d51fb7111
wangch <34704366@qq.com>
2022-10-18 00:28:10 +0800

    添加出错的页面

---
+++ src/components/ErrorPage.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="error">
+        <img src="https://img2.baidu.com/it/u=2545210976,4216576440&fm=253&fmt=auto&app=138&f=JPEG?w=600&h=500" alt="页面错误">
+        <div>路径{{errorMsg.path}}不存在，错误代码{{errorMsg.name}}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: [],
+    computed: {
+        errorMsg() {
+            const {path, name} = this.$route;
+            return {path, name}
+        }
+    },
+    methods: {
+        
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
