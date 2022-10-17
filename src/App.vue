208406ac076f0a63ae327199e08396c141f366d9
wangch <34704366@qq.com>
2022-10-17 10:22:52 +0800

    add the test of VueX

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
       <button @click="resetButtonClick">重置</button>
     </div>
     <ul>
-      <ToDoItem v-for="item,index in todoList" :key="index" class="todo-item">
+      <ToDoItem v-for="item, index in todoList" :key="index" class="todo-item">
 
         <!-- 命名插槽的使用语法 todo-item为插槽的name, 等号后面跟着是子组件传递出数据供父组件使用 -->
         <template v-slot:todo-item="itemProps">
@@ -25,6 +25,12 @@
     <div>
       添加次数：{{pushCount}} {{$store.getters.doubleCount}}
     </div>
+    <div>
+      <button type="button" @increment="console.log('123')" @click="$store.commit('pushAnyNumber', 11.3)">添加两次</button>
+      <button type="button" @click="$store.dispatch('pushTimeout')">延迟增加按钮</button>
+      <button type="button" @click="testButtonClick()">测试按钮</button>
+      <button type="button" @click="allocateStorage()">数组内存分配按钮</button>
+    </div>
   </div>
 </template>
 
@@ -40,7 +46,8 @@ export default {
   data() {
     return {
       todoList: ['hello'],
-      info: ''
+      info: '',
+      testStorage: [1,2,3],
     }
   },
   computed: {
@@ -81,6 +88,35 @@ export default {
       } else {
         alert('不能再重置了');
       }
+    },
+
+    testButtonClick() {
+      let a = [1,2,3,4,5]
+      a[10] = 123;
+      console.log(a);
+      // for (let i=0; i<a.length; i++) { console.log(a[i])};
+
+      let b = [1, ,,,,,undefined, undefined, undefined]
+      console.log(b)
+
+      console.log(b[3] === b[7], b[3] == b[7])
+
+    },
+
+    allocateStorage() {
+      this.testStorage[100000] = 100;
+      console.log(this.testStorage);
+      this.al = {
+        'name':'wang', 'gender' : 'man', "class" : 'master'
+
+      }
+    },
+
+    // jsx来渲染html标签
+    getJSXHtml (level) {
+      const Tag = `h${level}`;
+      // jsx语法
+      return <Tag>Hello World</Tag>
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -5,21 +5,36 @@ import App from './App.vue'
 Vue.config.productionTip = false
 Vue.use(Vuex)
 
+/*
+  传参对于多层嵌套非常繁琐且不易管理，代码维护相当麻烦，
+  vuex相当于把共享状态抽取出来，以全局单例模式来进行管理，
+  把共享的数据函数放进vuex，任何组件都可以进行使用
+*/
 // 创建store的时候记得用new操作符
 const store = new Vuex.Store({
   state: {
     pushCount: 0,
   },
 
-  // 调用时候用this.$store.commit("xxx函数")  来调用内部函数  
+  // 调用时候用this.$store.commit("xxx函数")  来调用内部函数 
   mutations: {
     pushOnce (state) {
       state.pushCount ++;
+    },
+
+    pushAnyNumber (state, number) {
+      if (Number(number)) {
+        state.pushCount += Math.floor(Number(number));
+      }
     }
   },
   // 执行异步操作，调用时候用this.$store.dispatch("xxx函数")  来调用内部函数，原理还是触发mutations方法  
   actions: {
-
+    pushTimeout({state}) {
+      setTimeout(() => {
+        state.pushCount += 1; 
+      }, 2000);
+    }
   },
   // 类似于computed，实现缓存
   getters: {
